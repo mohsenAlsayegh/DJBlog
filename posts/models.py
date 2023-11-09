e0ac639e377c49d1be977a8604b63551150b698c
Abdulmohsen Alsayegh <abdulmohsenal-sayegh@Abdulmohsens-MacBook-Air.local>
2023-11-09 19:37:21 +0300

    add publish_date

--- posts/models.py
+++ posts/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(max_length=20000)
     draft = models.BooleanField(default=True)
+    publish_date = models.DateTimeField(default=timezone.now)
     
 
     def __str__(self):
