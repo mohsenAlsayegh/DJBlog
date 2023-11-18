aa8ca9ff1544b6ba1efeeb1819d65f098fa2bd98
mohsenAlsayegh <macmuhsen@me.com>
2023-11-18 19:38:34 +0300

    add delete using CBV

--- posts/views.py
+++ posts/views.py
@@ -85,3 +85,7 @@ class EditPost(UpdateView):
     fields = '__all__'
     success_url = '/posts/'
     template_name = 'posts/edit.html'
+
+class DeletePost(DeleteView):
+    model = Post
+    success_url = '/posts/'
