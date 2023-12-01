6de370b20cb7d5f373301f08bcbb9b07b84e7e29
mohsenAlsayegh <macmuhsen@me.com>
2023-12-01 22:19:18 +0300

    show relationship as string in api

--- posts/serializers.py
+++ posts/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import Post 
 
 class PostSerializers(serializers.ModelSerializer):
+    author = serializers.StringRelatedField()
+    category = serializers.StringRelatedField()
     class Meta:
         model = Post
         fields = '__all__'
